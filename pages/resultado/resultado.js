c965bd63fb11d29c3fd96825253ce08fd5e3eafd
Giovanni229 barros de paula silva <giovanni.barros@eaportal.org>
2024-06-10 20:01:56 +0000

    adicionado a função de ensirir o resultado na tela

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -2,16 +2,12 @@ import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
-const assunto = localStorage.getItem("assunto")
-const botaoJogarNovamente = document.querySelector("main button")
+const assunto=localStorage.getItem("assuntos")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
-botaoJogarNovamente.addEventListener("click", jogarNovamente)
-
-verificarTema(body, botaoTema)
 
 function alterarAssunto() {
     const divIcone = document.querySelector(".assunto_icone")                                                                  
@@ -26,25 +22,4 @@ function alterarAssunto() {
 
 alterarAssunto()
 
-function inserirResultado(){
-    const sectionPontuacao = document.querySelector(".pontuacao")
-    const divAssunto = document.querySelector(".assunto")
-    const pontos = localStorage.getItem("pontos")
 
-    sectionPontuacao.innerHTML = `
-        ${divAssunto.outerHTML}
-
-        <strong>${pontos}</strong>
-
-        <p>de 10</p>
-    `
-}
-
-function jogarNovamente(){
-    localStorage.removeItem("pontos")
-    localStorage.removeItem("assunto")
-    
-    window.location.href = "../../index.html"
-}
-
-inserirResultado()

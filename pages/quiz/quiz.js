0c989c140b3a994e442c7f751c3ec9f873ffdf6c
Giovanni229 barros de paula silva <giovanni.barros@eaportal.org>
2024-06-10 19:55:57 +0000

    adicionadas as funções do botão de proxima pergunta e finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -150,11 +150,19 @@ function proximaPergunta() {
 
 function adicionarEventoInputs(){
     const inputsResposta = document.querySelectorAll(".alternativas input")
-    inputsResposta.forEach(input => {
-        input.addEventListener("click", guardarResposta)
+       input.addEventListener("click", guardarResposta)
 
         if (input.value === quiz.questions[pergunta-1].answer) {
             respostaCorretaId = input.id
         }
     })
 }
+
+async function iniciar() {
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
+}
+
+iniciar()

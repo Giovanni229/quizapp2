6a62b205fee667f5ef4bc3ec8ba8cbf39502b5c0
Giovanni229 barros de paula silva <giovanni.barros@eaportal.org>
2024-06-10 19:51:49 +0000

    adicionada a função de validar a resposta do usuario

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -112,3 +112,49 @@ function guardarResposta(evento){
     const botaoEnviar = document.querySelector(".alternativas button")
     botaoEnviar.addEventListener("click", validarResposta)
 }
+
+
+function validarResposta(){
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+
+    if (pergunta === 10){
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    }else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
+    if(resposta === quiz.questions[pergunta-1].answer){
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos + 1
+    }else{
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+    }
+
+    pergunta = pergunta + 1
+}
+
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
+
+    window.location.href = "../resultado/resultado.html"
+}
+
+function proximaPergunta() {
+    montarPergunta()
+    adicionarEventoInputs()
+}
+
+function adicionarEventoInputs(){
+    const inputsResposta = document.querySelectorAll(".alternativas input")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click", guardarResposta)
+
+        if (input.value === quiz.questions[pergunta-1].answer) {
+            respostaCorretaId = input.id
+        }
+    })
+}
